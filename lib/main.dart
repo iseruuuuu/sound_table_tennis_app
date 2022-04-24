6e4933481a8365c49682392e3015bf2399f073b4
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-24 17:24:42 +0900

    add: getxを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'home_screen.dart';
+import 'home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
