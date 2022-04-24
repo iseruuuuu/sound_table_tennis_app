1b7f07ef75b7d7325bf8684d61b87fbe41bdd148
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-24 17:26:36 +0900

    remove: remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'home_screen.dart';
 import 'home_screen/home_screen.dart';
 
 void main() {
