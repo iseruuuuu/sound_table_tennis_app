8300e0bca9108aabbfe8a94eeea4737910f6602c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-25 10:30:44 +0900

    add: gifのsplash screenを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'home_screen/home_screen.dart';
+import 'package:sound_table_teniss_app/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      // home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
